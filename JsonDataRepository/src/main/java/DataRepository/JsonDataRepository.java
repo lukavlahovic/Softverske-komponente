30113299b653984dfff00d7f2a6bbee906b5d04e
Luka Jelic <jelicl@ymail.com>
2020-10-26 11:52:21 +0100

    Update to branch2

--- JsonDataRepository/src/main/java/DataRepository/JsonDataRepository.java
+++ JsonDataRepository/src/main/java/DataRepository/JsonDataRepository.java
@@ -1,4 +1,22 @@
 package DataRepository;
 
-public class JsonDataRepository {
+import repository.DataRepository;
+
+import java.util.List;
+
+public class JsonDataRepository implements DataRepository {
+    @Override
+    public void save(String s, Object o) {
+
+    }
+
+    @Override
+    public <T> T findById(String s, String s1, Class<T> aClass) {
+        return null;
+    }
+
+    @Override
+    public <T> List<T> findAll(String s, Class<T> aClass) {
+        return null;
+    }
 }
